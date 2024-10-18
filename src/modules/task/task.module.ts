59833e20df40978a6d534d2ae9d9550288967738
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2024-10-18 17:30:14 -0300

    feat: find one task by id use case
    find only one task by id

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { FindOneByIdTaskController } from "./use-cases/find-one-by-id/find-one-by-id.controller";
+import { FindOneByIdTaskUseCase } from "./use-cases/find-one-by-id/find-one-by-id.use-case";
 import { FindAllTaskController } from "./use-cases/find-all/find-all.controller";
 import { FindAllTaskUseCase } from "./use-cases/find-all/find-all.use-case";
 import { CreateTaskController } from "./use-cases/create/create.controller";
@@ -9,7 +11,7 @@ import { TaskEntity } from "./models/entities/task.entity";
 
 @Module({
 	imports: [TypeOrmModule.forFeature([TaskEntity])],
-	controllers: [CreateTaskController, FindAllTaskController],
+	controllers: [CreateTaskController, FindAllTaskController, FindOneByIdTaskController],
 	providers: [
 		TaskTypeOrmRepository,
 		{
@@ -18,7 +20,8 @@ import { TaskEntity } from "./models/entities/task.entity";
 		},
 		CreateTaskUseCase,
 	    FindAllTaskUseCase,
+	    FindOneByIdTaskUseCase,
 	],
-	exports: ["ITaskRepository", CreateTaskUseCase, FindAllTaskUseCase],
+	exports: ["ITaskRepository", CreateTaskUseCase, FindAllTaskUseCase, FindOneByIdTaskUseCase],
 })
 export class TaskModule {}
--- src/modules/task/use-cases/find-all/find-all.spec.ts
+++ src/modules/task/use-cases/find-all/find-all.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { FindAllTaskUseCase } from "./find-all.use-case";
 import { ITaskRepository } from "../../models/interfaces/task-repository.interface";
+import { ListTaskDto } from "../../models/dtos/list-task.dto";
+import { TaskEntity } from "../../models/entities/task.entity";
+import { TaskStatus } from "../../models/enums/task-status.enum";
 
 describe("FindAllTask", () => {
 	let findAllUseCase: FindAllTaskUseCase;
 	let taskRepository: ITaskRepository;
-	let mockRepository = {};
+	let mockRepository = {
+		findAll: jest.fn().mockResolvedValue([new TaskEntity({ id: 1, title: "test", description: "test", status: TaskStatus.PENDING })]),
+	};
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -22,6 +27,17 @@ describe("FindAllTask", () => {
 	});
 
 	describe("FindAllTaskUseCase", () => {
-		
+		it("should be return a list of tasks", async () => {
+			const tasks = await findAllUseCase.execute();
+
+			expect(tasks).toBeInstanceOf(Array<ListTaskDto>);
+			expect(taskRepository.findAll).toHaveBeenCalledTimes(1);
+		});
+
+		it("should be return an empty array", () => {
+			jest.spyOn(taskRepository, "findAll").mockResolvedValueOnce([]);
+
+			expect(findAllUseCase.execute()).resolves.toEqual([]);
+		});
 	});
 });
